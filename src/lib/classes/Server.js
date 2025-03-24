bb8909dbd7427a929e2c093b33dff0b6d54b24fb
Carl W. Hansen <carlwonghansen@icloud.com>
2025-03-24 04:52:45 +0100

    format

--- src/lib/classes/Server.js
+++ src/lib/classes/Server.js
@@ -5,57 +5,56 @@ import cookie from '@fastify/cookie';
 import cors from '@fastify/cors';
 
 export class Server {
-  constructor(
-  {
-    mongodbURI
-  })
-  {
-    this.mongodbURI = mongodbURI;
-  };
+    constructor(
+        {
+            mongodbURI
+        }) {
+        this.mongodbURI = mongodbURI;
+    };
 
-  async start() {
-    try {
-      console.log("Attempting to connect to MongoDB.....");
+    async start() {
+        try {
+            console.log("Attempting to connect to MongoDB.....");
 
-      await mongoose
-                    .connect(this.mongodbURI)
-                    .then(() => console.log('Successfully connected to MongoDB!'))
-                    .catch(err => `An error has occured while attempting to connect to MongoDB! => ${err}`);
+            await mongoose
+                .connect(this.mongodbURI)
+                .then(() => console.log('Successfully connected to MongoDB!'))
+                .catch(err => `An error has occured while attempting to connect to MongoDB! => ${err}`);
 
-      console.log('Loading fastify plugins.....');
-         
-      await fastify(
-      {
-        logger: true
-      }
-      )
-             .register(cors)
-             .register(cookie,
+            console.log('Loading fastify plugins.....');
+
+            await fastify(
                 {
-                     secret: process.env.TOKEN_KEY,
-                     httpOnly: false,
-                })
-            .then(() => console.log('Successfully loaded the fastify plugins!'))
-            .catch(err => `An error has occured while attempting to load fastify plugins! => ${err}`);
+                    logger: true
+                }
+            )
+                .register(cors)
+                .register(cookie,
+                    {
+                        secret: process.env.TOKEN_KEY,
+                        httpOnly: false,
+                    })
+                .then(() => console.log('Successfully loaded the fastify plugins!'))
+                .catch(err => `An error has occured while attempting to load fastify plugins! => ${err}`);
 
-     console.log('attempting to load the routes.....');
-      await fastify()
-            .register(import('../../routes/index.js'))
-            .then(() => console.log('Successfully loaded the routes!'))
-            .catch(err => `An error has occured while attempting to load the routes! => ${err}`);
+            console.log('attempting to load the routes.....');
+            await fastify()
+                .register(import('../../routes/index.js'))
+                .then(() => console.log('Successfully loaded the routes!'))
+                .catch(err => `An error has occured while attempting to load the routes! => ${err}`);
 
-      console.log('Attempting to start the server.....');
-      
-      await fastify().listen({
-            port: 8080
-      })
-        .then(() => console.log('Successfully started the server! on port 8080'))
-        .catch(err => `An error has occured while attempting to start the server! => ${err}`);
-    }
+            console.log('Attempting to start the server.....');
 
-    catch (err) {
-      console.error(err);
-    }
+            await fastify().listen({
+                port: 8080
+            })
+                .then(() => console.log('Successfully started the server! on port 8080'))
+                .catch(err => `An error has occured while attempting to start the server! => ${err}`);
+        }
+
+        catch (err) {
+            console.error(err);
+        }
 
     }
 };
