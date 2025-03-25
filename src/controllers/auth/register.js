61ee14b4f7da67e82e2a0689fb2cc381d79bab83
Carl W. Hansen <carlwonghansen@icloud.com>
2025-03-25 11:20:17 +0100

    a

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,3 +1,7 @@
+// import 
+
+
+
 export default function register(fastify, options, done) {
     fastify.get('/api/auth/register', async (req, rep ) => {
         return rep.send({ message: 'Hello World' });
