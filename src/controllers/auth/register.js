baf9ff1fc1656618ee77fe1bd17aaf6a04a2392f
Carl W. Hansen <carlwonghansen@icloud.com>
2025-03-31 04:05:22 +0200

    format

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,46 +1,41 @@
 // Required modules.
 import parsePhoneNumber from 'libphonenumber-js';
 import bcrypt from 'bcrypt';
-//
+
 // Required schemas.
 import registerSchema from '../../schemas/auth/register.js';
-//
 
 // Required models.
 import User from '../../models/User.js';
-//
+
 export default function register(fastify, options, done) {
-    fastify.post('/api/v1/auth/register', async (req, rep ) => {
+    fastify.post('/api/v1/auth/register', async (req, rep) => {
         try {
             console.log('Attempting to validate the request body.....');
 
             await registerSchema
-            .validateAsync(req.body)
-            .then(() => {
-                console.log('Successfully validated the request body!');
-                return req.body;
-            })
-            .catch(err => {
-                console.error(`An error has occurred while attempting to validate the request body! => ${err}`);
-                throw err;
-            });
+                .validateAsync(req.body)
+                .then(() => {
+                    console.log('Successfully validated the request body!');
+                    return req.body;
+                })
+                .catch(err => {
+                    console.error(`An error has occurred while attempting to validate the request body! => ${err}`);
+                    throw err;
+                });
 
-             // Checks if the user already exists in the database.
-             console.log('Attempting to check if the user already exists in the database.....');
-             const doesUserExist = await User.findOne({
+            // Checks if the user already exists in the database.
+            console.log('Attempting to check if the user already exists in the database.....');
+            const doesUserExist = await User.findOne({
                 $or: [
                     { emailAddress: req.body.emailAddress },
                     { username: req.body.username },
                     { phoneNumber: req.body.phoneNumber }
                 ]
-             });
+            });
 
-             if (doesUserExist) {
-                const {
-                     emailAddress,
-                     username,
-                     phoneNumber
-                } = doesUserExist;
+            if (doesUserExist) {
+                const { emailAddress, username, phoneNumber } = doesUserExist;
 
                 const takenFields = [
                     emailAddress && `${emailAddress} is already taken!`,
@@ -49,19 +44,18 @@ export default function register(fastify, options, done) {
                 ].filter(Boolean);
 
                 return rep.status(409).send({ message: takenFields });
-             };
+            }
 
-             // parse phone number.
-             let parsedPhoneNumber = null;
-             if(req.local.body.phoneNumber) {
+            // Parse phone number.
+            let parsedPhoneNumber = null;
+            if (req.local.body.phoneNumber) {
                 const phoneNumber = req.local.body.phoneNumber;
-
                 parsedPhoneNumber = parsePhoneNumber(phoneNumber);
-             }
+            }
 
-             const encryptedPassword = await bcrypt.hash(req.local.body.password, 10);
+            const encryptedPassword = await bcrypt.hash(req.local.body.password, 10);
 
-             const user = await User.create({
+            const user = await User.create({
                 ...req.local.body,
                 phoneNumber: parsedPhoneNumber,
                 password: encryptedPassword,
@@ -70,14 +64,14 @@ export default function register(fastify, options, done) {
                         value: Math.random().toString(36).substring(2, 8)
                     }
                 }
-             });
+            });
 
-             console.log('Successfully created the user!');
-             return rep.status(201).send(user);
+            console.log('Successfully created the user!');
+            return rep.status(201).send(user);
         } catch (error) {
             return rep.status(500).send({ message: error.message });
-        };
-    })
+        }
+    });
 
     done();
-};
+}
